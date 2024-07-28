66727b28f76d59d510745f2e107f5553ad43fe1c
Aaron <joejoeleeder@gmail.com>
2024-07-27 22:46:35 -0400

    more phone fixes

--- client/phone.js
+++ client/phone.js
@@ -57,7 +57,25 @@ function connectWebSocket(token) {
     });
 }
 
+function toggleTracking() {
+    tracking ? stopTracking() : startTracking();
+}
 
+function startTracking() {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission().then(response => { 
+            if (response === 'granted') initSensors(); 
+        });
+    } else { 
+        initSensors(); 
+    }
+}
+
+function stopTracking() {
+    window.removeEventListener('devicemotion', handleMotion);
+    tracking = false;
+    document.getElementById('status').innerText = 'Not sending data';
+}
 
 function handleMotion(event) {
     if (!tracking) return;
@@ -263,6 +281,10 @@ function sendSensorData(calibratedQuaternion, position) {
     }
 }
 
-
+function initSensors() {
+    window.addEventListener('devicemotion', handleMotion);
+    tracking = true;
+    document.getElementById('status').innerText = 'Sending data';
+}
 
 connectWebSocket('your_test_token');
